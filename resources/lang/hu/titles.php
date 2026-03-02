1bf6b51d25074cae8f1b655d1a4b35c5a6b83295
istvan1243 <33933467+istvan1243@users.noreply.github.com>
2026-03-02 19:47:35 +0100

    null

--- resources/lang/hu/titles.php
+++ resources/lang/hu/titles.php
@@ -3,7 +3,9 @@
 return [
     "ts_FocusDefaultTheme_sidebar_maintenance" => "Karbantartás mód (tartalom) oldalsáv",
     "ts_FocusDefaultTheme_sidebar_top_nav"     => "Felső oldalsáv",
-    "ts_FocusDefaultTheme_sidebar_bottom_1"    => "Alsó oldalsáv 1",
-    "ts_FocusDefaultTheme_sidebar_bottom_2"    => "Alsó oldalsáv 2",
-    "ts_FocusDefaultTheme_sidebar_bottom_3"    => "Alsó oldalsáv 3",
+    "ts_FocusDefaultTheme_sidebar_bottom_1"    => "Alsó oldalsáv 1/1",
+    "ts_FocusDefaultTheme_sidebar_bottom_2"    => "Alsó oldalsáv 1/2",
+    "ts_FocusDefaultTheme_sidebar_bottom_3"    => "Alsó oldalsáv 1/3",
+    "ts_FocusDefaultTheme_sidebar_bottom_4"    => "Alsó oldalsáv 2",
+    "ts_FocusDefaultTheme_sidebar_head_source" => "HTML HEAD SOURCE",
 ];
