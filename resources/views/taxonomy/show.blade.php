536070df79ba62f9dcd5db0d6931de02db8a0e72
istvan1243 <33933467+istvan1243@users.noreply.github.com>
2026-05-16 00:27:04 +0200

    null

--- resources/views/taxonomy/show.blade.php
+++ resources/views/taxonomy/show.blade.php
@@ -11,7 +11,7 @@
     @endif
 
     <div class="mb-0 py-12 font-semibold bg-slate-100 text-black w-full flex justify-items-center">
-        <h1 class="inline-block mx-auto text-3xl">{{ $term->title }}</h1>
+        <h1 class="inline-block mx-auto text-3xl">{{ $term->localizedTitle(cms_locale()) }}</h1>
     </div>
 
     @if(!empty($meta['description'] ?? null))
